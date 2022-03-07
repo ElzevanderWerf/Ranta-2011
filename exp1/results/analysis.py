d249e3520e35d6fc7e1ee32d71349e8e3919e7e4
Elze van der Werf <e.g.vanderwerf@students.uu.nl>
2022-03-07 15:25:21 +0100

    updating analysis

--- exp1/results/analysis.py
+++ exp1/results/analysis.py
@@ -2,10 +2,14 @@
 TSV file per response."""
 
 import pandas as pd
+import statistics as stat
 
 responses = 1  # number of responses, TODO change
 
-DFs = [pd.read_csv("1." + str(i) + ".tsv", sep="\t", header=0) for i in range(1,responses+1)]
+DFs = [pd.read_csv("results/1." + str(i) + ".tsv", sep="\t", header=0) for i in range(1,responses+1)]
+# The shape of each df in DFs is: (1,131)
+#   1: 1 response
+#   131: Time stamp + Q1 + Q2 + Q3 + 25 * (Q4 + Q5 + Q6 + Q7 + Q8) + Q9 + Q10
 
 questions ={1:"What is your gender?",
             2:"How old are you?",
@@ -19,21 +23,41 @@ questions ={1:"What is your gender?",
             10:"Do you have any final comments?"
             }
 
-# Gender
+# GENDER
 genders = [df.at[0, questions[1]] for df in DFs]
+print("Gender\n\tMale:", genders.count("Male"), genders.count("Male") / len(genders))
+print("\tFemale:", genders.count("Female"), genders.count("Female") / len(genders))
+print("\tPrefer not to say:", genders.count("Prefer not to say"), genders.count("Prefer not to say") / len(genders))
 
-# Age
+# AGE
 ages = [df.at[0, questions[2]] for df in DFs]
+print("\n\nAge\n\tMean:", stat.mean(ages))
+print("\tSD:", stat.stdev(ages))
 
-# Experience
+# EXPERIENCE
 experiences = [df.at[0, questions[3]] for df in DFs]
+print("\n\nExperience\n\tMean:", stat.mean(experiences))
+print("\tSD:", stat.stdev(experiences))
 
-# Correctness?
+# FILLER CORRECTNESS
+fillers = [df.iloc[:,104:129].at[0, questions[4]] for df in DFs]
 
-# Clarity
-clarities = [df.at[0, questions[6]] for df in DFs] 
+# For the next questions, I only look at the NON-FILLER items
+# CORRECTNESS
+correctnesses = [df.iloc[:,4:104].at[0, questions[4]] for df in DFs]
+print("\n\nCorrectness\n\tYes:", genders.count("Yes"), genders.count("Yes") / len(genders))
+print("\tNo:", genders.count("No"), genders.count("No") / len(genders))
+
+# CLARITY
+clarities = [df.iloc[:,4:104].at[0, questions[6]] for df in DFs] 
 #will this return multiple values bc multiple columns have this name?
+print("\n\nClarity")
 
-# Fluency
-fluencies = [df.at[0, questions[7]] for df in DFs] 
+# FLUENCY
+fluencies = [df.iloc[:,4:104].at[0, questions[7]] for df in DFs] 
 #will this return multiple values bc multiple columns have this name?
+print("\n\nFluency")
+
+# POST-EDITS
+edits = [df.iloc[:,4:104].at[0, questions[8]] for df in DFs] 
+print("\n\nPost-edits")
