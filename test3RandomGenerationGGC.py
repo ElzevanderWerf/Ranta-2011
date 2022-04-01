8385a54a45586647693628d43795cffbce241080
Elze van der Werf <e.g.vanderwerf@students.uu.nl>
2022-04-01 09:51:20 +0200

    remove unnecessary depth parameter from makeAtom fun

--- test3RandomGenerationGGC.py
+++ test3RandomGenerationGGC.py
@@ -29,11 +29,11 @@ class RandomGenerator:
         because then it will not be parsable by the GF shell.
         """
         if n == 0:          # termination signal: atomic proposition
-            return self.makeAtom(n-1, bindsVars)
+            return self.makeAtom(bindsVars)
         else: 
             i = r.choice(range(7))
             if i == 0:      # atomic proposition
-                return self.makeAtom(n-1, bindsVars)
+                return self.makeAtom(bindsVars)
             
             elif i == 1:    # negation
                 return "~ " + self.makeProp(n-1, bindsVars)
@@ -64,7 +64,7 @@ class RandomGenerator:
                 var, j = self.makeVar(afterQ=True)
                 return "/ " + var + " " + self.makeProp(n-1, bindsVars.union([j]))
     
-    def makeAtom(self, n, bound=set()):
+    def makeAtom(self, bound=set()):
         """
         Randomly generate an atomic proposition from the lexicon,
         with maximum depth n. bound is a set of variable integers that are 
@@ -128,29 +128,29 @@ ggcLexicon = Lexicon(["Small", "Medium", "Large", "Even"],      #Pred1s
                         ["Dodec", "Student", "Cube", "Prime", "Person", 
                          "Tet", "Pet"])                 #Kinds
 
-# # Test to generate 1 proposition        
-# rg = RandomGenerator(ggcLexicon, 3)  
-# random_formula = rg.makeProp(3)
-# print(random_formula)
+# Test to generate 1 proposition        
+rg = RandomGenerator(ggcLexicon, 3)  
+random_formula = rg.makeProp(3)
+print(random_formula)
 
 
-# Randomly generate 1000 formulas with a certain maximum depth, that are
-# not too long and not too short
-depth = 3       # maximum depth
-formulas = []
-rg = RandomGenerator(ggcLexicon, depth)
+# # Randomly generate 1000 formulas with a certain maximum depth, that are
+# # not too long and not too short
+# depth = 3       # maximum depth
+# formulas = []
+# rg = RandomGenerator(ggcLexicon, depth)
 
-while len(formulas) < 1000:
-    prop = rg.makeProp(depth, set())
-    if len(prop) >= 20 and len(prop) <= 100:
-        formulas.append(prop)
-    rg.new_var_i = 0    #set again to 0 for generating a new prop
-    rg.new_cons_i = 0   #set again to 0 for generating a new prop
+# while len(formulas) < 1000:
+#     prop = rg.makeProp(depth, set())
+#     if len(prop) >= 20 and len(prop) <= 100:
+#         formulas.append(prop)
+#     rg.new_var_i = 0    #set again to 0 for generating a new prop
+#     rg.new_cons_i = 0   #set again to 0 for generating a new prop
 
-# Write formulas to file with newlines
-with(open(r'out/test3GGC.tmp', 'w')) as f:
-    f.write('\n'.join(formulas))
-f.close()
+# # Write formulas to file with newlines
+# with(open(r'out/test3GGC.tmp', 'w')) as f:
+#     f.write('\n'.join(formulas))
+# f.close()
 
         
         
